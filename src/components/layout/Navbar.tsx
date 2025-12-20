a97bdadd32bf56ea13459f68abfd418c11e51bc5
Java-Refer <jitendrapal.pal@gmail.com>
2025-12-21 00:04:05 +0530

    Update job label to 'Europe Job Consultancy'

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -231,7 +231,7 @@ const Navbar: React.FC = () => {
                 <Briefcase className="w-5 h-5 text-white" />
               </div>
               <span className="text-xl font-bold text-gray-900">
-                Europe Job
+                Europe Job Consultancy
               </span>
             </Link>
 
